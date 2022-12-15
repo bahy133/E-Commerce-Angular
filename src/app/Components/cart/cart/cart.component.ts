d49b8ecb8a5819e338cdb8fd2403a7b1b20c9a61
bahy133 <bahy713@gmail.com>
2022-12-15 15:11:42 +0200

    13th commit

--- src/app/Components/cart/cart/cart.component.ts
+++ src/app/Components/cart/cart/cart.component.ts
@@ -9,6 +9,7 @@ export class CartComponent implements OnInit {
   cartItems: any[] = [];
   Cartexist: boolean = true;
   total: number = 0;
+  order: boolean = false;
   constructor() {}
 
   ngOnInit(): void {
@@ -22,8 +23,40 @@ export class CartComponent implements OnInit {
     }
   }
   calcTotal() {
+    this.total = 0;
     for (let i = 0; i < this.cartItems.length; i++) {
       this.total += this.cartItems[i].item.price * this.cartItems[i].quantity;
     }
+    this.total = Math.round(this.total * 10) / 10;
+  }
+  updateTotal(event: any, id: number) {
+    this.calcTotal();
+    this.cartItems.filter((item) => item.item.id == id)[0].quantity =
+      event.target.value;
+    this.save();
+  }
+  save() {
+    localStorage.setItem('cart', JSON.stringify(this.cartItems));
+  }
+  removeItem(index: number) {
+    this.cartItems.splice(index, 1);
+    this.calcTotal();
+    this.save();
+  }
+  increase(index: number) {
+    this.cartItems[index].quantity++;
+    this.calcTotal();
+    this.save();
+  }
+  decrease(index: number) {
+    this.cartItems[index].quantity--;
+    this.calcTotal();
+    this.save();
+  }
+  clearCart() {
+    localStorage.removeItem('cart');
+    this.cartItems = [];
+    this.total = 0;
+    this.Cartexist = false;
   }
 }
