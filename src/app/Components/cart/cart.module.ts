15d301dd26c22c34523fe3e58054f908985f864b
bahy133 <bahy713@gmail.com>
2022-12-15 14:08:58 +0200

    12th commit

--- src/app/Components/cart/cart.module.ts
+++ src/app/Components/cart/cart.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [{ path: '', component: CartComponent }];
 @NgModule({
   declarations: [CartComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
 })
 export class CartModule {}
--- src/app/Components/cart/cart/cart.component.ts
+++ src/app/Components/cart/cart/cart.component.ts
@@ -8,14 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
   cartItems: any[] = [];
   Cartexist: boolean = true;
+  total: number = 0;
   constructor() {}
 
   ngOnInit(): void {
     if ('cart' in localStorage) {
       this.cartItems = JSON.parse(localStorage.getItem('cart')!);
       this.Cartexist = true;
+      this.calcTotal();
     } else {
       this.Cartexist = false;
+      this.total = 0;
+    }
+  }
+  calcTotal() {
+    for (let i = 0; i < this.cartItems.length; i++) {
+      this.total += this.cartItems[i].item.price * this.cartItems[i].quantity;
     }
   }
 }
